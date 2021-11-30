79057ddd4cdf044541301d74fbd42867ae59a30c
Matthieu CORNUT-CHAUVINC <mcornutchauvinc@gmail.com>
2021-11-30 15:44:01 +0100

    adds 3ds2 required object

--- types/payment.go
+++ types/payment.go
@@ -27,6 +27,7 @@ type PaymentAuthorise3dParams struct {
 type PaymentAuthoriseForRecurringParams struct {
 	AdditionalData   *AdditionalData    `json:"additionalData"` // 3D Secure
 	Amount           *Amount            `json:"amount"`
+	BrowserInfo      *BrowserInfo       `json:"browserInfo"`
 	Reference        string             `json:"reference"`
 	ShopperEmail     string             `json:"shopperEmail"`
 	ShopperIP        string             `json:"shopperIP"`
@@ -65,6 +66,37 @@ type PaymentAuthoriseRecurringParams struct {
 	Recurring                        *RecurringContract `json:"recurring"`
 	ShopperInteraction               InteractionType    `json:"shopperInteraction"` // ContAuth
 	MerchantAccount                  string             `json:"merchantAccount"`
+}
+
+// BrowserInfo is the shopper's browser information, for 3D Secure, the full object is required for web integrations.
+type BrowserInfo struct {
+
+	// The accept header value of the shopper's browser.
+	AcceptHeader string `json:"acceptHeader"`
+
+	// The color depth of the shopper's browser in bits per pixel. This should be obtained by using the browser's screen.colorDepth property. Accepted values: 1, 4, 8, 15, 16, 24, 30, 32 or 48 bit color depth.
+	ColorDepth int `json:"colorDepth"`
+
+	// Boolean value indicating if the shopper's browser is able to execute Java.
+	JavaEnabled bool `json:"javaEnabled"`
+
+	// Boolean value indicating if the shopper's browser is able to execute JavaScript. A default 'true' value is assumed if the field is not present.
+	JavaScriptEnabled bool `json:"javaScriptEnabled"`
+
+	// The navigator.language value of the shopper's browser (as defined in IETF BCP 47).
+	Language string `json:"language"`
+
+	// The total height of the shopper's device screen in pixels.
+	ScreenHeight int `json:"screenHeight"`
+
+	// The total width of the shopper's device screen in pixels.
+	ScreenWidth int `json:"screenWidth"`
+
+	// Time difference between UTC time and the shopper's browser local time, in minutes.
+	TimeZoneOffset int `json:"timeZoneOffset"`
+
+	// The user agent value of the shopper's browser.
+	UserAgent string `json:"userAgent"`
 }
 
 /********************************************/
